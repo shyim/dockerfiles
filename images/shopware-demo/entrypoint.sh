5c124df4843fb5c5d66a3d6f52b4f72f2fed916f
Soner Sayakci <s.sayakci@shopware.com>
2019-02-12 15:54:15 +0100

    Fix host

--- images/shopware-demo/entrypoint.sh
+++ images/shopware-demo/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+/usr/bin/mysqld_safe --datadir='/var/lib/mysql' &
+sleep 5
+
+
+mysql -h 127.0.0.1 shopware -e "UPDATE s_core_shops set host = '${VIRTUAL_HOST}'"
+mysql -h 127.0.0.1 shopware -e "UPDATE s_core_shops set secure = '${VIRTUAL_SSL}'"
+rm -rf /var/www/html/var/cache/*
+
 /usr/bin/supervisord -c /etc/supervisord.conf
