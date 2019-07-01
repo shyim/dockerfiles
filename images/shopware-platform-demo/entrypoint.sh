98391001e6c69c69274505371dafd884aa72a437
Soner Sayakci <s.sayakci@gmail.com>
2019-07-01 21:27:39 +0200

    Update platform demo

--- images/shopware-platform-demo/entrypoint.sh
+++ images/shopware-platform-demo/entrypoint.sh
@@ -1,56 +1,64 @@
-#!/bin/sh
+#!/bin/bash
 
 /usr/bin/mysqld --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mysql/plugin --user=mysql & 
 sleep 5
 
 SCHEME="http"
 
-if [[ $VIRTUAL_SSL == "1" ]]; then
+if [[ ${VIRTUAL_SSL} == "1" ]]; then
     sed -i -e 's/<?php/<?php $_SERVER["HTTPS"] = true;/g' /var/www/html/public/index.php
     SCHEME="https"
 fi
 
 php /var/www/html/bin/console sales-channel:create:storefront --url="${SCHEME}://${VIRTUAL_HOST}"
 
-php /var/www/html/bin/console framework:demodata
-php /var/www/html/bin/console dbal:refresh:index
+if [[ $GENERATE_DEMODATA == "1" ]]; then
+    php /var/www/html/bin/console framework:demodata
+    php /var/www/html/bin/console dbal:refresh:index
+fi;
 
-if [[ $DISABLE_BACKEND == "1" ]]; then
+if [[ ${DISABLE_BACKEND} == "1" ]]; then
 	mkdir /var/www/html/admin
 fi
 
+if [[ ! -z ${PLUGIN_GIT_URL} ]]; then
+    IFS=',' read -r -a GIT_URLS <<< "$PLUGIN_GIT_URL"
+    IFS=',' read -r -a GIT_NAMES <<< "$PLUGIN_GIT_NAMES"
 
-if [[ ! -z $PLUGIN_GIT_URL ]]; then
-    name=$(basename $PLUGIN_GIT_URL)
-    name=$(echo $name | sed 's/\.git//g')
+    for index in "${!GIT_URLS[@]}"
+    do
+        FOLDERNAME=$(basename ${GIT_URLS[index]})
+        FOLDERNAME=$(echo ${FOLDERNAME} | sed 's/\.git//g')
 
-    echo "Checking out plugin: $name"
+        NAME=${GIT_NAMES[index]}
+        echo "Checking out plugin: ${NAME}"
 
-    cd /var/www/html/custom/plugins
-    git clone $PLUGIN_GIT_URL
+        cd /var/www/html/custom/plugins
+        git clone ${GIT_URLS[index]}
 
-    if [[ ! -z $PLUGIN_GIT_COMMIT ]]; then
-        echo "Checking out commit: $PLUGIN_GIT_COMMIT"
-        git checkout $PLUGIN_GIT_COMMIT
-    fi
+        cd ${FOLDERNAME}
 
-    cd $name
-    if [[ -e composer.json ]]; then
-        composer install -o --no-dev
-    fi
+        /var/www/html/bin/console plugin:refresh
+        /var/www/html/bin/console plugin:install ${NAME} --activate
 
-    /var/www/html/bin/console plugin:refresh
-    /var/www/html/bin/console plugin:install $INSTALL_NAME --activate
+        if [[ -e src/Resources/demo.sql ]]; then
+            mysql shopware < src/Resources/demo.sql
+        fi
 
-    if [[ -e src/Resources/demo.sql ]]; then
-    	mysql shopware < src/Resources/demo.sql
-    fi
+        if [[ -e src/Resources/demo.sh ]]; then
+            bash src/Resources/demo.sh
+        fi
 
-    if [[ -e src/Resources/demo.sh ]]; then
-        bash src/Resources/demo.sh
-    fi
+        cd /var/www/html/
+    done
 fi
 
 rm -rf /var/www/html/var/cache/*
+
+php /var/www/html/bin/console assets:install
+
+rm -rf /var/www/html/var/cache/*
+
+chown -R 1000:1000 /var/www/html
 
 /usr/bin/supervisord -c /etc/supervisord.conf
